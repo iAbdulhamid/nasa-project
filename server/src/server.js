d86b82f74f8e613f817c423b158d948cbffb7373
Ibrahim Abdelhamed <i_abdulhamid@outlook.com>
2022-08-09 23:15:34 +0200

    GET planets endpoint added

--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,10 @@
 const http = require('http');
 const app = require('./app');
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8000;  // in pakage.json: "start": "set PORT=5000 && node src/server.js"
 
 const server = http.createServer(app);
+
 
 
 
