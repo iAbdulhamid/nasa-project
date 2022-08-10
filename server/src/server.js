6910feec1796b94c579cb3152b324e8117a831b9
Ibrahim Abdelhamed <i_abdulhamid@outlook.com>
2022-08-10 23:44:33 +0200

    loading planets data on startup

--- server/src/server.js
+++ server/src/server.js
@@ -1,15 +1,21 @@
 const http = require('http');
 const app = require('./app');
 
+const { loadPlanetsData } = require('./models/planets.model');
+
 const PORT = process.env.PORT || 8000;  // in pakage.json: "start": "set PORT=5000 && node src/server.js"
 
 const server = http.createServer(app);
 
 
 
+async function startServer() {
+    await loadPlanetsData();
 
+    server.listen(PORT, () => {
+        console.log(`Listening on port ${PORT}...`);
+    });
+}
 
+startServer();
 
-server.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}...`);
-});
