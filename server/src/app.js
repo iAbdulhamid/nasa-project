91fa04341766ad0f6f4bc8c37712198155a7a50a
Ibrahim Abdelhamed <i_abdulhamid@outlook.com>
2022-08-13 13:04:14 +0200

    serving react.js front-end in production

--- server/src/app.js
+++ server/src/app.js
@@ -1,3 +1,4 @@
+const path    = require('path');
 const express = require('express');
 const cors    = require('cors');
 
@@ -10,8 +11,14 @@ app.use(cors({
 }));
 
 app.use(express.json());
-app.use(planetsRouter)
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
+
+
+app.use(planetsRouter)
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
 
 
 
